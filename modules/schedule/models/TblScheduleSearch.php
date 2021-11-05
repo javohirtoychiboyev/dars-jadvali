485710ee0dff2aa52f121e3fd2756540827e253e
Javohir <javohirtoychiboyev1997@gmail.com>
2021-11-05 14:12:14 +0500

    search optimallashtirildi

--- modules/schedule/models/TblScheduleSearch.php
+++ modules/schedule/models/TblScheduleSearch.php
@@ -52,31 +52,22 @@ class TblScheduleSearch extends TblSchedule
         $this->load($params);
 
         if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
+
             return $dataProvider;
         }
         if($this->begin_time){
-            $query->andFilterWhere(['<=', 'begin_time', $this->begin_time])
-                  ->andFilterWhere(['>=', 'end_time', $this->begin_time]);
+            $query->andFilterWhere(['<=', 'begin_time', '09:10'])
+                  ->andFilterWhere(['>=', 'end_time', '09:10']);
         }
-        //\yii\helpers\VarDumper::dump($query,10,true); die;
-        // grid filtering conditions
+
         $query->andFilterWhere([
-            'id' => $this->id,
             'teachers_id' => $this->teachers_id,
             'subjects_id' => $this->subjects_id,
             'teachers_rel_subjects_id' => $this->teachers_rel_subjects_id,
             'rooms_id' => $this->rooms_id,
             'groups_id' => $this->groups_id,
             'week_days_id' => $this->week_days_id,
-            'begin_time' => $this->begin_time,
-            'reg_date' => $this->reg_date,
             'status' => $this->status,
-            'created_at' => $this->created_at,
-            'created_by' => $this->created_by,
-            'updated_at' => $this->updated_at,
-            'updated_by' => $this->updated_by,
         ]);
 
         return $dataProvider;
